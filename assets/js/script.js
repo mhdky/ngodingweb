7b8a03e63bf81b6528cebb7c3d59073a39342526
mhdky <83162131+mhdky@users.noreply.github.com>
2022-06-26 00:34:40 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// burger button section 
 const listBurgerBtn = document.querySelector(".list-burger-btn");
 listBurgerBtn.addEventListener("click", () => {
     const containerListLeft = document.querySelector(".container-list-left");
@@ -9,24 +10,31 @@ listLeftNameSection.addEventListener("click", () => {
     containerListLeft.classList.remove("container-list-left-show");
 });
 
+
+// notifikasi section
 const btnNotifikasiDua = document.querySelector(".btn-notifikasi-dua");
 btnNotifikasiDua.addEventListener("click", () => {
     const notifikasiDua = document.querySelector(".notifikasi-dua");
     notifikasiDua.style.display = "flex"
-    // notifikasiDua.style.height = "100vh";
 });
 const closeNotifikasiDua = document.querySelector(".close-notifikasi-dua");
 closeNotifikasiDua.addEventListener("click", () => {
     const notifikasiDua = document.querySelector(".notifikasi-dua");
     notifikasiDua.style.display = "none"
-    // notifikasiDua.style.width = "0";
-    // notifikasiDua.style.height = "0";
 });
-
 const btnNotifikasiLeftDua = document.querySelector(".btn-notifikasi-dua-left");
 btnNotifikasiLeftDua.addEventListener("click", () => {
     const notifikasiDua = document.querySelector(".notifikasi-dua");
     notifikasiDua.style.display = "flex"
-    // notifikasiDua.style.width = "100%";
-    // notifikasiDua.style.height = "100vh";
+});
+
+
+// daftar isi section 
+const btnDaftarIsi = document.querySelector(".btn-daftar-isi");
+btnDaftarIsi.addEventListener("click", () => {
+    const containerDaftarIsi = document.querySelector(".container-daftar-isi");
+    const btnDaftarIsiI = document.querySelector(".btn-daftar-isi i");
+
+    btnDaftarIsiI.classList.toggle("btnDaftarIsiI-rotate");
+    containerDaftarIsi.classList.toggle("container-daftar-isi-hide");
 });
